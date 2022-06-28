7ab1a21c64ffb28882102e06c59ec05cc140c3fe
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2022-06-28 19:39:56 +0300

    add base repository

---
+++ CheckoutApi.WebApp/CheckoutApi.DataAccess/Repositories/Concrete/Base/BaseRepository.cs
@@ -0,0 +1,66 @@
+﻿using CheckoutApi.DataAccess.Contect.Interfaces;
+using CheckoutApi.DataAccess.Helpers;
+using CheckoutApi.DataAccess.Repositories.Interfaces.Base;
+using CheckoutApi.DataModels.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CheckoutApi.DataAccess.Repositories.Concrete.BaseRepository
+{
+    public abstract partial class BaseRepository<TEntity, TPrimaryKey> : IBaseRepository<TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        protected ICheckoutDbContext CheckoutDbContext { get; }
+        protected DbSet<TEntity> DbSet { get; }
+
+        protected BaseRepository(ICheckoutDbContext checkoutDbContext)
+        {
+            CheckoutDbContext = checkoutDbContext;
+            DbSet = checkoutDbContext.Set<TEntity>();
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void Remove(TEntity entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            DbSet.AddRange(entities);
+        }
+
+        public virtual async Task<TEntity> GetByAsync(IEnumerable<Expression<Func<TEntity, bool>>> whereFilters,
+           Expression<Func<TEntity, object>>[] includes = null,
+           bool asNoTracking = false)
+        {
+            return await DbSet
+              .IncludeMultiple(includes)
+              .ConfigureAsNoTracking(asNoTracking)
+              .CombineWhereFilters(whereFilters)
+              .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, object>>[] includes = null, bool asNoTracking = false)
+        {
+            return await DbSet
+             .IncludeMultiple(includes)
+             .ConfigureAsNoTracking(asNoTracking)
+             .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllByAsync(IEnumerable<Expression<Func<TEntity, bool>>> whereFilters,
+            Expression<Func<TEntity, object>>[] includes = null,
+            bool asNoTracking = false)
+        {
+            return await DbSet.IncludeMultiple(includes)
+              .CombineWhereFilters(whereFilters)
+              .ConfigureAsNoTracking(asNoTracking)
+              .ToListAsync();
+        }
+    }
+}
---
+++ CheckoutApi.WebApp/CheckoutApi.DataAccess/Repositories/Interfaces/Base/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CheckoutApi.DataModels.Base;
+using System.Linq.Expressions;
+
+namespace CheckoutApi.DataAccess.Repositories.Interfaces.Base
+{
+    public interface IBaseRepository<TEntity, TPrimaryKey> where TEntity : class, IEntity<TPrimaryKey>
+    {
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+
+        Task<TEntity> GetByAsync(IEnumerable<Expression<Func<TEntity, bool>>> whereFilters,
+         Expression<Func<TEntity, object>>[] includes = null,
+         bool asNoTracking = false);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, object>>[] includes = null,
+             bool asNoTracking = false);
+
+        Task<IEnumerable<TEntity>> GetAllByAsync(IEnumerable<Expression<Func<TEntity, bool>>> whereFilters,
+            Expression<Func<TEntity, object>>[] includes = null,
+            bool asNoTracking = false);
+
+    }
+}
