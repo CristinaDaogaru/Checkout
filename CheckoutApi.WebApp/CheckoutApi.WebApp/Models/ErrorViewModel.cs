f2ef1fcf77489b580677d890c1d27b8259301aa8
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2022-06-28 17:38:35 +0300

    initial project structure

---
+++ CheckoutApi.WebApp/CheckoutApi.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CheckoutApi.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
