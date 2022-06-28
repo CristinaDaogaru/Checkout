81434e9161a514175cf143b0b5a32a122421a29b
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2022-06-28 19:07:14 +0300

    configure dbcontext

---
+++ CheckoutApi.WebApp/CheckoutApi.DataAccess/Context/Concrete/CheckoutDbContext.cs
@@ -0,0 +1,38 @@
+﻿using CheckoutApi.DataAccess.Contect.Interfaces;
+using CheckoutApi.DataModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckoutApi.DataAccess.Contect.Concrete
+{
+    public class CheckoutDbContext : IdentityDbContext, ICheckoutDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<ItemsInBasket> ItemsInBaskets { get; set; }
+
+        public CheckoutDbContext(string connectionString) : base(new DbContextOptionsBuilder().UseSqlServer(connectionString).Options)
+        {
+        }
+        public CheckoutDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ItemsInBasket>()
+                .HasKey(itemsInBasket => new { itemsInBasket.BasketId, itemsInBasket.ItemId });
+
+            modelBuilder.Entity<ItemsInBasket>()
+                .HasOne(itemsInBasket => itemsInBasket.Item)
+                .WithMany(item => item.ItemsInBaskets);
+
+            modelBuilder.Entity<ItemsInBasket>()
+                .HasOne(itemsInBasket => itemsInBasket.Basket)
+                .WithMany(basket => basket.ItemsInBaskets);
+        }
+    }
+}
---
+++ CheckoutApi.WebApp/CheckoutApi.DataAccess/Context/Interfaces/ICheckoutDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CheckoutApi.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckoutApi.DataAccess.Contect.Interfaces
+{
+    public interface ICheckoutDbContext : IDisposable
+    {
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Item> Items { get; set; }
+        DbSet<Basket> Baskets { get; set; }
+        DbSet<ItemsInBasket> ItemsInBaskets { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
