7ecd0f50f7c9f8bed310758f2e3048ebe5509fa8
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2022-06-28 19:58:13 +0300

    change id type to int

--- CheckoutApi.WebApp/CheckoutApi.DataModels/Customer.cs
+++ CheckoutApi.WebApp/CheckoutApi.DataModels/Customer.cs
@@ -2,9 +2,9 @@
 
 namespace CheckoutApi.DataModels
 {
-    public class Customer : IEntity<string>
+    public class Customer : IEntity<int>
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string FullName { get; set; }
         public bool PaysVAT { get; set; }
     }
