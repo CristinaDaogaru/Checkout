106b069b00dd3f91dd1ad02fb8cf2710077068be
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2022-06-29 14:02:11 +0300

    update migration remove unnecessary field

---
+++ CheckoutApi.WebApp/CheckoutApi.DataAccess/Migrations/20220629104142_UpdateMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckoutApi.DataAccess.Migrations
+{
+    public partial class UpdateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalSpentAmount",
+                table: "Baskets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSpentAmount",
+                table: "Baskets",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
